cdfb8a6c4e6d5c2472f99beaa6b7324c73cd7e70
Minus-hates-sch <78946158+Minus-hates-school@users.noreply.github.com>
2022-05-04 13:32:27 +0000

    *Indent type: to tab* *fixed indention error* *Fix for indention: Highlight all of error code, CTR+[ then CTR+], CMD if on Mac*

--- main.py
+++ main.py
@@ -66,49 +66,6 @@ async def on_message(message):
 		
 	if message.content == ('!fakeperson'):
 		await message.channel.send(BotFuncs.fakePerson(),reference=message)
-		
-  if "https://discord.com/channels" in message.content:
-    text = message.content
-    l = text.replace(", ", " ").split(" ")
-    key = "https://discord.com/channels/"
-    for item in l:
-        if key in item:
-            try:
-                lilink = l[l.index(item)]
-                response = "-**---** **Content** **in** **the** **link** **above** **---**- \n\n"
-                link = lilink.replace("https://discord.com/channels/", "").split("/")
-                sourceServer = client.get_guild(int(link[0]))
-                sourceChannel = sourceServer.get_channel(int(link[1]))
-                sourceMessage = await sourceChannel.fetch_message(int(link[2]))
-
-                if len(sourceMessage.content) <= 1000:
-                    embed = discord.Embed(title=response, description="", timestamp=datetime.datetime.utcnow())
-                    embed.add_field(name=f"Length: {len(sourceMessage.content)}", value=sourceMessage.content)
-                    embed.set_footer(text=sourceMessage.author, icon_url=sourceMessage.author.avatar_url)
-                    await message.channel.send(embed=embed)
-
-                if len(sourceMessage.content) > 1000:
-                    contents = sourceMessage.content
-                    con2 = []
-                    splitstr = math.ceil(len(contents) / 1000)
-                    embed1 = discord.Embed(title=response, description="", timestamp=datetime.datetime.utcnow())
-                    while contents:
-                        con2.append(contents[:900])
-                        contents = contents[900:]
-                    for feilds in range(0, splitstr):
-                        embed1.add_field(name="**---CUT---HERE---**", value=f"```py\n{con2[feilds]}\n```",
-                                         inline=False)
-                    embed1.set_footer(text=sourceMessage.author, icon_url=sourceMessage.author.avatar_url)
-                    await message.channel.send(embed=embed1)
-
-            except:
-
-                await message.channel.send(f"         -**Cannot** **preview**-\n-"
-                                           f"**Make sure message is in this server,"
-                                           f" and not a text file or image**-")
-
-	
-	
 	
 	if message.content.startswith('!embed') == True:
 		text = message.content
@@ -124,7 +81,45 @@ async def on_message(message):
 		dm = await message.author.create_dm()  # Creates a dm channel with the user
 		await dm.send("What you want to send")
 
-
+	if "https://discord.com/channels" in message.content:
+		text = message.content
+		l = text.replace(", ", " ").split(" ")
+		key = "https://discord.com/channels/"
+		for item in l:
+				if key in item:
+						try:
+								lilink = l[l.index(item)]
+								response = "-**---** **Content** **in** **the** **link** **above** **---**- \n\n"
+								link = lilink.replace("https://discord.com/channels/", "").split("/")
+								sourceServer = client.get_guild(int(link[0]))
+								sourceChannel = sourceServer.get_channel(int(link[1]))
+								sourceMessage = await sourceChannel.fetch_message(int(link[2]))
+	
+								if len(sourceMessage.content) <= 1000:
+										embed = discord.Embed(title=response, description="", timestamp=datetime.datetime.utcnow())
+										embed.add_field(name=f"Length: {len(sourceMessage.content)}", value=sourceMessage.content)
+										embed.set_footer(text=sourceMessage.author, icon_url=sourceMessage.author.avatar_url)
+										await message.channel.send(embed=embed)
+	
+								if len(sourceMessage.content) > 1000:
+										contents = sourceMessage.content
+										con2 = []
+										splitstr = math.ceil(len(contents) / 1000)
+										embed1 = discord.Embed(title=response, description="", timestamp=datetime.datetime.utcnow())
+										while contents:
+												con2.append(contents[:900])
+												contents = contents[900:]
+										for feilds in range(0, splitstr):
+												embed1.add_field(name="**---CUT---HERE---**", value=f"```py\n{con2[feilds]}\n```",
+																				 inline=False)
+										embed1.set_footer(text=sourceMessage.author, icon_url=sourceMessage.author.avatar_url)
+										await message.channel.send(embed=embed1)
+	
+						except:
+	
+								await message.channel.send(f"         -**Cannot** **preview**-\n-"
+																					 f"**Make sure message is in this server,"
+																					 f" and not a text file or image**-")
 
 
 if __name__ == "__main__":
